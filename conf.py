bc0e189e0afd6b9bd3db40fb4ba2f05361e52052
SamsonWang <zl.wang@foxmail.com>
2018-04-25 15:07:14 +0800

    do not use CDN

--- conf.py
+++ conf.py
@@ -1238,7 +1238,7 @@ INDEX_DISPLAY_POST_COUNT = 5
 # Bootstrap is served from BootstrapCDN (provided by MaxCDN)
 # Set this to False if you want to host your site without requiring access to
 # external resources.
-USE_CDN = True
+USE_CDN = False
 
 # Check for USE_CDN compatibility.
 # If you are using custom themes, have configured the CSS properly and are
