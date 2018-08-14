e2ffbd671c98fb247c63acd1a88d205ed85d4931
SamsonWang <zl.wang@foxmail.com>
2018-08-14 17:01:27 +0800

    fix up link problem

--- plugins/orgmode/init.el
+++ plugins/orgmode/init.el
@@ -109,12 +109,23 @@ contextual information."
           (pygmentize (downcase lang) (org-html-decode-plain-text code))
         code-html))))
 
-;; Export images with custom link type
+;; Export images with custom img link type
 (defun org-custom-link-img-url-export (path desc format)
   (cond
    ((eq format 'html)
     (format "<img src=\"%s\" alt=\"%s\"/>" path desc))))
-(org-add-link-type "img-url" nil 'org-custom-link-img-url-export)
+(org-link-set-parameters "img-url"
+                         :follow nil
+                         :export 'org-custom-link-img-url-export)
+
+;; Expand internal link
+(defun org-custom-link-internal-url-export (path desc format)
+  (cond
+   ((eq format 'html)
+    (format "<a href=\"%s\">%s</a>" path desc))))
+(org-link-set-parameters "link"
+                         :follow nil
+                         :export 'org-custom-link-internal-url-export)
 
 ;; Export function used by Nikola.
 (defun nikola-html-export (infile outfile)
