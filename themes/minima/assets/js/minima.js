4b30941f4d1f9f2b06c5b20e2b64bdeaa1747859
Samson Wang <wangzhilv@gmail.com>
2020-05-04 15:45:28 +0800

    auto scroll navbar

--- themes/minima/assets/js/minima.js
+++ themes/minima/assets/js/minima.js
@@ -1,30 +1,42 @@
 
-function initScrollTop () {
+var initNavbar = function () {
+  // hide nav when scroll down, show nav when scroll up
+  var updateNav = function () {
+    var previousTop = 0;
+    function updateNavImpl () {
+      var currentTop = window.pageYOffset;
+      // console.log("initNavbar, previousTop =", previousTop);
+      // console.log("initNavbar, currentTop =", currentTop);
+      if (currentTop + 30 < previousTop || currentTop < 60) { // scrolling top
+        document.querySelector(".nav-custom").classList.remove("hidden");
+      }
+      else if (currentTop > previousTop) {
+        document.querySelector(".nav-custom").classList.add("hidden");
+      }
+      previousTop = currentTop;
+    }
+    return updateNavImpl;
+  } ();
+  window.addEventListener("scroll", updateNav);
+}
+
+var initScrollTop = function () {
   document.querySelector("#scroll-top")
     .addEventListener("click", function() {
-      // console.log("scroll-up clicked");
       window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
     });
 
   window.addEventListener("scroll", function() {
-//    scrollObject = {
-//       x: window.pageXOffset,
-//       y: window.pageYOffset
-//    }
-//  console.log("window scrolled", scrollObject);
-
     if (window.pageYOffset > 800) {
       document.querySelector("#scroll-top").style = "display:block;";
-//  console.log("window scrolled, set display block");
     }
     else {
       document.querySelector("#scroll-top").style = "display:none;";
-//      console.log("window scrolled, set display none");
     }
   });
-}
+};
 
-function modAnchorTarget () {
+var modAnchorTarget = function () {
   document.querySelectorAll('a').forEach(function(item) {
     // console.log("modAnchortarget,", item);
     var pattern = new RegExp("/" + window.location.host + "/");
@@ -33,18 +45,19 @@ function modAnchorTarget () {
       item.target = "_blank";
     }
   });
-}
+};
 
-function initLazyLoad () {
+var initLazyLoad = function () {
   var lazyLoadInstance = new LazyLoad({
     elements_selector: "img[data-src]"
   });
-}
+};
 
 // Handler when the DOM is fully loaded
 var onReady = function() {
-  initScrollTop();
+  initNavbar();
   modAnchorTarget();
+  initScrollTop();
   initLazyLoad();
 };
 
@@ -56,8 +69,9 @@ if (document.readyState === "complete"
   document.addEventListener("DOMContentLoaded", onReady);
 }
 
-
-// avoid css transition on load
+/*
 window.addEventListener("load", function() {
+  // avoid css transition before load
   document.querySelector("body").classList.remove("preload");
 });
+*/
