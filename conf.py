79146ddb5d56a7717bed960b83a8778649598755
Samson Wang <wangzhilv@gmail.com>
2021-11-23 14:16:57 +0800

    update explore page

--- conf.py
+++ conf.py
@@ -1110,7 +1110,7 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code',
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-INDEX_DISPLAY_POST_COUNT = 6
+INDEX_DISPLAY_POST_COUNT = 12
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
